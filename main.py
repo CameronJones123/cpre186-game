1514265a1dc1f0d57e793d9fcc37cb7d1b8b7001
CameronJones123 <cpjones@iastate.edu>
2021-03-02 12:57:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import and initialize the pygame library
 import pygame
 import Player
+import map
 pygame.init()
 from pygame.locals import (
     RLEACCEL,
@@ -13,7 +14,31 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
-
+class block(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(block, self).__init__()
+        self.surf = pygame.Surface((50, 50))
+        self.surf.fill((0, 0, 0))
+        self.rect = self.surf.get_rect()
+        self.rect.right = x
+        self.rect.top = y
+class map:
+    load_list = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]]
+    x = 50
+    y = 500
+    def inti(self):
+        super(map, self).__init__()
+    def load(self):
+        for i in range(len(self.load_list)):
+            self.y -= 50
+            self.x = 50
+            for j in range(len(self.load_list[i])):
+                if (self.load_list[i][j] == 1):
+                    new_Block = block(self.x, self.y)
+                    blocks.add(new_Block)
+                    all_sprites.add(new_Block)
+                print(self.load_list[i][j])
+                self.x += 50
 # Set up the drawing window
 screen = pygame.display.set_mode([500, 500])
 
@@ -22,7 +47,10 @@ DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 # Run until the user asks to quit
 all_sprites = pygame.sprite.Group()
 player = Player.player()
+blocks = pygame.sprite.Group()
 all_sprites.add(player)
+map = map()
+map.load()
 #initializes the clock
 clock = pygame.time.Clock()
 running = True
