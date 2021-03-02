92f5511650f304b4297dd70918b6074e04230f00
CameronJones123 <79165771+CameronJones123@users.noreply.github.com>
2021-03-02 12:19:54 -0600

    Add files via upload

---
+++ map.py
@@ -0,0 +1,12 @@
+import pygame
+class map:
+    def inti(self):
+        super(map, self).__init__()
+        self.tiles[] = [0,0,0,0,0]
+        self.intialx = 50
+        self.intialy = 50
+
+    def load(self):
+        
+
+
