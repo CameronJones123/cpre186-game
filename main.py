ac3685435da6aa4c16c86199dfbdfeec678c5459
CameronJones123 <cpjones@iastate.edu>
2021-03-09 12:30:15 -0600

    added improved skeleton

--- main.py
+++ main.py
@@ -1,46 +1,96 @@
-# Import and initialize the pygame library
-import pygame
-import Player
-import map
-pygame.init()
-from pygame.locals import *
+# KidsCanCode - Game Development with Pygame video series
+# Tile-based game - Part 2
+# Collisions and Tilemaps
+# Video link: https://youtu.be/ajR4BZBKTr4
+import pygame as pg
+import sys
+from os import path
+from settings import *
+from sprites import *
 
-# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+        self.load_data()
 
-DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
 
-# Run until the user asks to quit
-all_sprites = pygame.sprite.Group()
-player = Player.player()
-blocks = pygame.sprite.Group()
-all_sprites.add(player)
-map1 = map.map()
-map1.load(blocks,all_sprites)
-#initializes the clock
-clock = pygame.time.Clock()
-running = True
-while running:
-    #player.update(pressed_keys)
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == KEYDOWN:
-            # Was it the Escape key? If so, stop the loop.
-            if event.key == K_ESCAPE:
-                running = False
-        pressed_keys = pygame.key.get_pressed()
-        player.update(pressed_keys)
+    def new(self):
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
 
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
-    for entity in all_sprites:
-        screen.blit(entity.surf, entity.rect)
-    clock.tick(30)
-    # Flip the display
-    pygame.display.flip()
+    def quit(self):
+        pg.quit()
+        sys.exit()
 
-# Done! Time to quit.
-pygame.quit()
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+# create the game object
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
