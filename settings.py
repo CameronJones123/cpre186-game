c601fd1c8cd69dfe4cbcf51e075afc0f345339e5
Isaac Vrba <isaac.vrba@gmail.com>
2021-04-06 12:42:09 -0500

    Quality of life changes

--- settings.py
+++ settings.py
@@ -15,9 +15,8 @@ SKYBLUE = (135, 206, 235)
 WIDTH = 1024   #a large enough number that occupies the screen and also divisible by 32 so we have full tiles
 HEIGHT = 768  #same logic as the width, but for height
 FPS = 144
-TITLE = "CprE 186 Game"	#text at the top of the window when the program runs
-#BGCOLOR = DARKGREY
-BACKGROUND = pg.image.load("background.jpg") #loads in our background
+TITLE = "CprE 186 Game"
+BACKGROUND = pg.image.load("background.jpg")
 
 TILESIZE = 32  #size of the tile that we chose
 GRIDWIDTH = WIDTH / TILESIZE
